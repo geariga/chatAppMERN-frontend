63a7b0c223c4bd4599e06c96a61d966b45479621
Andrew Gearig <geariga9@gmail.com>
2021-07-22 13:13:37 -0400

    added ellipsis for text overflow in chat rooms list

--- src/components/ChatRoomListItem.js
+++ src/components/ChatRoomListItem.js
@@ -2,13 +2,18 @@ import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Avatar from './Avatar'
 
-export default function ChatRoomListItem(props) {
+export default function ChatRoomListItem({roomName, lastMessage}) {
     return (
-        <ListGroup.Item variant="light" className="py-4" action>
+        <ListGroup.Item 
+            onClick={() => console.log('suh')}
+            variant="light" 
+            className="chatroom-li py-4" 
+            action
+        >
             <Avatar />
             <div>
-                <h6>{props.roomName}</h6>
-                <p>{props.lastMessage}</p>
+                <h6>{roomName}</h6>
+                <p>{lastMessage}</p>
             </div>
         </ListGroup.Item>
     )
--- src/components/ChatRoomsList.js
+++ src/components/ChatRoomsList.js
@@ -3,9 +3,23 @@ import ChatRoomListItem from './ChatRoomListItem'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 export default function ChatRoomsList() {
+    const chats = new Array(15).fill({
+        roomName: 'Test Room',
+        lastMessage: 'this was the final message sent to the chat room.' +
+            ' this is intentionally a long string used for testing' +
+            ' this is intentionally a long string used for testing' +
+            ' this is intentionally a long string used for testing' +
+            ' this is intentionally a long string used for testing'
+    })
     return (
         <ListGroup>
-
+            {chats.map((chat, i) => {
+                return <ChatRoomListItem 
+                    key={`${chat.roomName}${i}`}
+                    roomName={chat.roomName}
+                    lastMessage={chat.lastMessage}
+                />
+            })}
         </ListGroup>
     )
 }
