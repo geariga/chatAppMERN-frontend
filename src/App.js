0063366d3cfbf0fe97c2fc23beaf41a6641bd07d
Andrew Gearig <geariga9@gmail.com>
2021-07-29 18:36:32 -0400

    Get stored token on page load

--- src/App.js
+++ src/App.js
@@ -1,80 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { UserContext } from './contexts/user'
-import { socket, SocketContext } from './contexts/socket'
-import InitialModal from './components/InitialModal'
+import { SocketProvider } from './contexts/SocketProvider'
+import { ChatProvider } from './contexts/ChatProvider'
+import LoginWindow from './components/LoginWindow'
 import SideBar from './components/SideBar'
-import SideBarNav from './components/SideBarNav'
-import ChatRoomsList from './components/ChatRoomsList'
 import ContentWindow from './components/ContentWindow'
 import ContentWindowNav from './components/ContentWindowNav'
 import ChatWindow from './components/ChatWindow'
 import MessageInput from './components/MessageInput'
-import ChatRoomSearch from './components/ChatRoomSearch'
+
+function getStoredToken() {
+    return localStorage.getItem('wat010203')
+}
 
 export default function App() {
-    const [showModal, setShowModal] = useState(false)
-    const [currentUser, setCurrentUser] = useState(null)
-
-    const getUserIfTokenIsValid = async () => {
-        const token = localStorage.getItem('wat010203')
-        if (token === null || token === 'null') {
-            console.log('no token')
-            setShowModal(() => true)
-            return null
-        } else {
-            const tokenObject = JSON.parse(token)
-            const currentTime = new Date().getTime()
-            if (tokenObject.expiration <= currentTime) {
-                console.log('expired token')
-                localStorage.setItem('wat010203', null)
-                setShowModal(() => true)
-                return null
-            } else {
-                console.log('token is valid')
-                const response = await axios.post('/api/getuser', {}, {
-                    headers: {
-                        'Authorization': `Bearer ${tokenObject.token}`
-                    }
-                })
-                return response.data
-            }
-        }
-    }
+    console.log('app rendered')
+    const [token, setToken] = useState(getStoredToken())
 
     useEffect(() => {
-        (async function () {
-            const user = await getUserIfTokenIsValid()
-            if (user) {
-                setCurrentUser(user)
-                return user
-            }
-        })()
-    }, [])
+        localStorage.setItem('wat010203', token)
+    }, [token])
 
-    return <>
-        <InitialModal
-            show={showModal}
-            onHide={() => setShowModal(false)}
-            setShowModal={setShowModal}
-        />
-        <SocketContext.Provider value={socket}>
-            <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-                <SideBar>
-                    <SideBarNav />
-                    <ChatRoomSearch />
-                    <ChatRoomsList />
-                </SideBar>
+    const userInterface = (
+        <SocketProvider token={token}>
+            <ChatProvider>
+                <SideBar />
                 <ContentWindow>
                     <ContentWindowNav />
-                    <ChatWindow
-                        socket={socket}
-                        // messages={messages}
-                        user={currentUser}
-                    />
+                    <ChatWindow />
                     <MessageInput />
                 </ContentWindow>
-            </UserContext.Provider>
-        </SocketContext.Provider>
-    </>
+            </ChatProvider>
+        </SocketProvider>
+    )
+
+    return (
+        token ? userInterface : <LoginWindow setToken={setToken} />
+    )
 }
